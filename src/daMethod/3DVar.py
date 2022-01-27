0f9c96634bbff4f458b5257b8596472df8b5e808
neko2048 <show.999991@gmail.com>
2022-01-28 01:44:11 +0800

    fix: import package right

--- src/daMethod/3DVar.py
+++ src/daMethod/3DVar.py
@@ -1,11 +1,20 @@
+# daMethod/variationalMethods.py
+# Fu-Sheng Kao
+# >>>>>>>>>> imported packages >>>>>>>>>>
 import numpy as np
 from scipy.integrate import ode
 import copy
 from scipy.optimize import minimize
 
-from initValueGenerate import Lorenz96
+from pathlib import Path
+import sys
+path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
+sys.path.insert(0, path)
+from dataGenerate import initValueGenerate
 from parameterControl import *
-from dataRecorder import RecordCollector
+from dataRecord.dataRecorder import RecordCollector
+# <<<<<<<<<< imported packages <<<<<<<<<<
+
 
 class threeDVar:
     def __init__(self, xInitAnalysis):
@@ -42,47 +51,37 @@ class threeDVar:
                         method='CG', jac=self.gradientOfCostFunction).x
         return analysisState
 
-if __name__ == "__main__":
-    # states
-    xInitAnalysis = np.loadtxt("{}/initRecord/{}/initAnalysisState.txt".format(observationOperatorType, subFolderName))
-    xFullObservation = np.loadtxt("{}/initRecord/{}/sparseObservationState.txt".format(observationOperatorType, subFolderName))
-    xTruth = np.loadtxt("{}/initRecord/{}/sparseTruthState.txt".format(observationOperatorType, subFolderName))
 
-    # covariance 
-    analysisEC = np.loadtxt("{}/initRecord/{}/initEC.txt".format(observationOperatorType, subFolderName))
-    if "full" in observationOperatorType:
-        observationEC = np.identity(Ngrid) * (noiseScale ** 2)
-    else:
-        observationEC = np.identity(int(Ngrid/2)) * (noiseScale ** 2)
 
-    # collector
-    dataRecorder = RecordCollector(methodName="threeDVar", noiseType=noiseType)
-    if not dataRecorder.checkDirExists():
-        dataRecorder.makeDir()
 
-    # initial setup
-    threeDvar = threeDVar(xInitAnalysis)
-    threeDvar.analysisState = xInitAnalysis
-    threeDvar.analysisEC = analysisEC
-    threeDvar.forecastState = xInitAnalysis # presumed
-    threeDvar.forecastEC = analysisEC # presumed
-    threeDvar.observationState = xFullObservation[0]
-    threeDvar.observationEC = observationEC
-    threeDvar.RMSE = np.sqrt(np.mean((threeDvar.analysisState - xTruth[0])**2))
-    threeDvar.MeanError = np.mean(threeDvar.analysisState - xTruth[0])
-    dataRecorder.record(threeDvar, tidx=0)
-    print("{:02f}: {:05f}".format(0, threeDvar.RMSE))
-    for tidx, nowT in enumerate(timeArray[:-1]):
-        threeDvar.observationState = xFullObservation[tidx+1]
 
-        threeDvar.forecastState = threeDvar.getForecastState(threeDvar.analysisState, nowT=nowT)
-        threeDvar.analysisState = threeDvar.getAnalysisState(backgroundState = threeDvar.forecastState, \
-                                                             observationState = threeDvar.observationState, \
-                                                             backgroundEC = threeDvar.forecastEC, \
-                                                             observationEC = threeDvar.observationEC)
 
-        threeDvar.RMSE = np.sqrt(np.mean((threeDvar.analysisState - xTruth[tidx+1])**2))
-        threeDvar.MeanError = np.mean(threeDvar.analysisState - xTruth[tidx+1])
-        dataRecorder.record(threeDvar, tidx=tidx+1)
-        print("{:02f}: {:05f}".format(nowT+dT, threeDvar.RMSE))
-    dataRecorder.saveToTxt()
+
+
+
+
+class increThreeDVar:
+    def __init__(self, xInitAnalysis):
+        self.xInitAnalysis = xInitAnalysis
+        self.obsOperator = np.loadtxt("{}/initRecord/observationOperator.txt".format(observationOperatorType))
+
+    def costFunction(self, analysisIncrement, innovation, backgroundEC, observationEC):
+        H = self.obsOperator
+        backgroundCost = (analysisIncrement).transpose() @ np.linalg.inv(backgroundEC) @ (analysisIncrement)
+
+        observationCost = (H @ analysisIncrement - innovation).transpose() @ np.linalg.inv(observationEC) @ (H @ analysisIncrement - innovation)
+        return 0.5 * (backgroundCost + observationCost)
+
+    def gradientOfCostFunction(self, analysisIncrement, innovation, backgroundEC, observationEC):
+        H = self.obsOperator
+        gradientBackgroundCost = np.linalg.inv(backgroundEC) @ (analysisIncrement)
+
+        gradientObservationCost = H.transpose() @ np.linalg.inv(observationEC) @ (H @ analysisIncrement - innovation)
+        return gradientBackgroundCost + gradientObservationCost
+
+    # forecast
+    def getForecastState(self, analysisState, nowT):
+        lorenz = Lorenz96(initValue=analysisState)
+        lorenz.solver = lorenz.getODESolver(initTime=nowT)
+        backgroundState = lorenz.solveODE(endTime=nowT+dT)
+        return backgroundState
---
+++ src/daMethod/EKF.py
@@ -0,0 +1,73 @@
+import numpy as np
+from scipy.integrate import ode
+
+from pathlib import Path
+import sys
+path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
+sys.path.insert(0, path)
+from parameterControl import *
+from lorenz96.lorenz96 import Lorenz96
+from dataRecord.dataRecorder import RecordCollector
+
+class ExtKalFilter:
+    def __init__(self, xInitAnalysis):
+        self.xInitAnalysis = xInitAnalysis
+        self.obsOperator = np.loadtxt("{}/initRecord/observationOperator.txt".format(observationOperatorType))
+
+    def forceODE(self, x, force=force):
+        return (np.roll(x, -1) - np.roll(x, 2)) * np.roll(x, 1) - x + force
+
+    # forecasting
+    def getForecastState(self, analysisState, startTime):
+        lorenz = Lorenz96(initValue=analysisState)
+        lorenz.solver = lorenz.getODESolver(initTime=startTime)
+        forecastState = lorenz.solveODE(endTime=startTime+dT)
+        return forecastState
+
+    def getForcastEC(self, analysisState, analysisEC):
+        jacobianM = self.getJacobianOfMfromTransition(analysisState)
+        forecastEC = jacobianM @ analysisEC @ jacobianM.transpose()
+        return forecastEC
+
+    def getJacobianOfForceODE(self, xState):
+        """F(t_i)"""
+        jacobianOfForceMaxtrix = np.zeros((xState.shape[0], xState.shape[0]), dtype=float)
+        x_n = xState
+        x_np1 = np.roll(xState, -1) # x_n+1
+        x_nm1 = np.roll(xState, +1) # x_n-1
+        x_nm2 = np.roll(xState, +2) # x_n-2
+        for i in range(len(xState)):
+            tempState = np.array([-x_nm1[i], x_np1[i] - x_nm2[i], -1, x_nm1[i]])
+            jacobianOfForceMaxtrix[i, :4] = tempState
+            jacobianOfForceMaxtrix[i] = np.roll(jacobianOfForceMaxtrix[i], -2+i)
+        return jacobianOfForceMaxtrix
+
+    def getJacobianOfMfromTransition(self, analysisState, Nsplit=10):
+        tempState = analysisState
+        ddT = dT / Nsplit
+        M = np.identity(len(tempState))
+        for i in range(Nsplit):
+            F = self.getJacobianOfForceODE(xState = tempState)
+            L_i = np.identity(Ngrid) + ddT * F
+            tempState = tempState + ddT * self.forceODE(x=tempState)
+            M = L_i @ M
+        return M
+
+    # analyzing
+    def getAnalysisWeight(self, forecastState, forecastEC, observationEC):
+        H = self.obsOperator
+        inverseMatrix = np.linalg.inv(H @ forecastEC @ (H.transpose()) + observationEC)
+        K = forecastEC @ (H.transpose()) @ (inverseMatrix)
+        return K
+
+    def getAnalysisState(self, forecastState, observationState, KalmanGain):
+        H = self.obsOperator
+        innovation = (observationState - H @ forecastState)
+        analysisState = forecastState + (KalmanGain @ innovation)
+        return analysisState
+
+    def getAnalysisEC(self, forecastEC, KalmanGain, inflation=1):
+        H = self.obsOperator
+        analysisEC = (np.identity(Ngrid) - (KalmanGain @ H)) @ forecastEC
+        analysisEC = inflation * analysisEC
+        return analysisEC
